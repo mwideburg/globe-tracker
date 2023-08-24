d290a6eccc409b453aa7bd0087c401403303f195
Michael Wideburg <mwideburg@gmail.com>
2023-08-23 22:30:09 -0400

    fix(iss): fix altitude

--- src/components/globe/Globe.js
+++ src/components/globe/Globe.js
@@ -48,9 +48,9 @@ function EarthGlobe() {
       const phi = (90 - lat) * (Math.PI / 180);
       const theta = (lon + 180) * (Math.PI / 180);
 
-      const x = -(radius * Math.sin(phi) * Math.cos(theta));
-      const y = radius * Math.cos(phi);
-      const z = (radius * Math.sin(phi) * Math.sin(theta)) + ISS_ALTITUDE
+      const x = -((radius + ISS_ALTITUDE) * Math.sin(phi) * Math.cos(theta));
+      const y = (radius + ISS_ALTITUDE) * Math.cos(phi); 
+      const z = (radius + ISS_ALTITUDE) * Math.sin(phi) * Math.sin(theta);
 
       return new THREE.Vector3(x, y, z);
     }
